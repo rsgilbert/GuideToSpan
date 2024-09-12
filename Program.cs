071ec716b63e22d9fc7c2f452232575478fa475e
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-09-12 04:06:09 +0300

    looked at string.Create function

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
 ﻿var a = Enumerable.Range(1, 5).ToArray();
 // Console.WriteLine(WorkWithSum.Sum(a, 2, 2));
 Span<int> s = new Span<int>(a, 2, 2);
-Console.WriteLine(WorkWithSum.Sum(s));
+// Console.WriteLine(WorkWithSum.Sum(s));
 
+// unsafe
+// {
+//     int* ptr = stackalloc int[2000];
+//     WorkWithSum.Sum(new Span<int>(ptr, 1000));
+// }
+
+
+string st = string.Create(34, Guid.NewGuid(), (span, s) => 
+{
+     Console.WriteLine(span.ToString());
+    span[0] = 'M';
+    span[1] = 'T';
+    span[2] = 'Z';
+    Console.WriteLine(s);
+    Console.WriteLine(span.ToString());
+   s.TryFormat(span[2..], out _, "N");
+    Console.WriteLine(span.ToString());
+});
+Console.WriteLine(st.Length);
+Console.WriteLine("X"+st+"X");
